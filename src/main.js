dd6b98c2292e397ca1f67e518d1ec32ca2a02f40
shad kamil hama saeed <54715323+shadKamel@users.noreply.github.com>
2024-09-09 11:44:21 +0300

    a little update

--- src/main.js
+++ src/main.js
@@ -3,19 +3,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-const store = createStore({
-  state() {
-    return {
-      count: 0,
-    };
-  },
-  mutations: {
-    increment(state) {
-      state.count++;
-    },
-  },
-});
+import { store } from "./store";
 
 const app = createApp(App);
 
